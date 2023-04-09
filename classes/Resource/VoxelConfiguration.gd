6fe5df7880a5e56def63e35d409134713620ffdc
Daniel Herkert <daniel.herkert@gmail.com>
2023-04-09 13:08:05 +0300

    Added UV helper tools and descriptions

--- classes/Resource/VoxelConfiguration.gd
+++ classes/Resource/VoxelConfiguration.gd
@@ -7,54 +7,79 @@ class_name VoxelConfiguration
 signal voxel_configuration_changed(what)
 
 
+## Material dictionary for voxels. VoxelData.data is referring to these array indices.
 @export var materials : Array[Material]:
 	set(v):
 		materials = v
 		emit_signal("voxel_configuration_changed", "materials")
 
-# Voxel size in game units
-@export_range(0.01, 32.0, 0.01) var voxel_base_size : float = 1.0:
+## Voxel reference size in game units. Does not change actual mesh or voxel sizes, but can be used to scale world entities, gravity etc. from one place.
+## Voxel to mesh size is alway 1:1 to keep mesh gewneration as fast as possible.
+@export_range(0.01, 32.0, 0.01) var voxel_reference_size : float = 1.0:
 	set(v):
-		voxel_base_size = v
-		emit_signal("voxel_configuration_changed", "voxel_base_size")
+		voxel_reference_size = v
+		emit_signal("voxel_configuration_changed", "voxel_reference_size")
 
-# Mesh generation mode
+## Mesh generation strategy to use
 enum MESH_MODE {
 	NONE,
 	CUBES,
 	FACES,
 	FAST
 }
+## Mesh generation strategy to use
 @export var mesh_mode : MESH_MODE = MESH_MODE.FAST:
 	set(v):
 		mesh_mode = v
 		emit_signal("voxel_configuration_changed", "mesh_mode")
 
-# Select hardware acceleration mode to use, will fall back to NONE if not available on target system
+## Hardware acceleration mode to use, will fall back to NONE if not available on target system
 enum ACCEL_MODE {
 	NONE,
 	NATIVE,
 	GPU,
 	GPU_AND_NATIVE
 }
+## Hardware acceleration mode to use, will fall back to NONE if not available on target system
 @export var accel_mode : ACCEL_MODE = ACCEL_MODE.NONE:
 	set(v):
 		accel_mode = v
 		emit_signal("voxel_configuration_changed", "accel_mode")
 		
 
-# Select hardware acceleration mode to use, will fall back to NONE if not available on target system
+## Threading mode to use, will fall back to NONE if not available on target system
 enum THREAD_MODE {
 	NONE,
 	SIMPLE,
 	TASKSERVER
 }
+## Threading mode to use, will fall back to NONE if not available on target system
 @export var thread_mode : THREAD_MODE = THREAD_MODE.NONE:
 	set(v):
 		thread_mode = v
 		emit_signal("voxel_configuration_changed", "thread_mode")
 
-
+@export_group("Helper tools")
+## Select which material index in the materials array to edit
+@export var material_to_edit = 0:
+	set(nv):		
+		material_to_edit = clamp(nv,0,materials.size())
+## Doubles the material size by dividing its Triplanar UV1 and UV2 size by two. This is helpfull to circumvent Godot Editors automatic rounding to nearest 0.001 decimals.
+@export var double_material_size : bool = false:
+	set(nv):
+		if nv:
+			print("Doubled the uv size of material %s" % material_to_edit)
+			double_material_size = false
+			materials[material_to_edit].uv1_scale /= 2
+			materials[material_to_edit].uv2_scale /= 2
+## Halves the material size by doubling its Triplanar UV1 and UV2 size. This is helpfull to circumvent Godot Editors automatic rounding to nearest 0.001 decimals.
+@export var half_material_size : bool = false:
+	set(nv):
+		if nv:
+			print("Halved the uv size of material %s" % material_to_edit)
+			half_material_size = false
+			materials[material_to_edit].uv1_scale *= 2
+			materials[material_to_edit].uv2_scale *= 2
 func _init():
 	pass
 	# TODO: test if fixed
