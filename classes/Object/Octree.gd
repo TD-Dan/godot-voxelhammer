3c66a639eced7df80e4d24c8c696072c27e52ee0
Daniel Herkert <daniel.herkert@gmail.com>
2023-06-04 15:37:24 +0300

    fixed wrong rounding on negative coordinate in .get_branch(...)

--- classes/Object/Octree.gd
+++ classes/Object/Octree.gd
@@ -69,7 +69,8 @@ func _init():
 	branch_cache.resize(cache_size)
 
 var cache_rotating_index = 0
-func get_branch(position : Vector3i, max_depth = -1, create_missing = true) -> OctreeBranch:
+func get_branch(positionf : Vector3, max_depth = -1, create_missing = true) -> OctreeBranch:
+	var position = Vector3i(positionf.floor())
 	if cache_size > 0:
 		for cached_branch in branch_cache:
 			if cached_branch and cached_branch.position == position:
@@ -102,6 +103,26 @@ func get_branch(position : Vector3i, max_depth = -1, create_missing = true) -> O
 		return found_root
 	
 	return null
+
+var octet_positions = [
+	Vector3(0,0,0),
+	Vector3(1,0,0),
+	Vector3(0,1,0),
+	Vector3(1,1,0),
+	Vector3(0,0,1),
+	Vector3(1,0,1),
+	Vector3(0,1,1),
+	Vector3(1,1,1),
+]
+
+func create_nearest_octet_neighbours(position : Vector3, branch, apply_to_parents = true):
+	var sides = Vector3i(-1,-1,-1)
+	if position.x > float(branch.position.x)+branch.size/2.0: sides.x = 1
+	if position.y > float(branch.position.y)+branch.size/2.0: sides.y = 1
+	if position.z > float(branch.position.z)+branch.size/2.0: sides.z = 1
+	
+	for octpos in octet_positions:
+		get_branch(Vector3i(Vector3(branch.position)+octpos*Vector3(sides).snapped(Vector3(branch.size, branch.size, branch.size))), branch.level)
 
 
 func clear():
